a4b839bd2183e0dc612eb6cb87bee5c4a58ba4ec
Robert D. Winter, 2nd <robert.d.winter.ctr@mail.mil>
2018-10-07 08:35:24 -0500

    feat(prototype): Giter8 template
    Template for creating new Lagom services.
    
    #NONE

--- src/main/g8/common/src/main/scala/$package$/common/regex/Matchers.scala
+++ src/main/g8/common/src/main/scala/$package$/common/regex/Matchers.scala
@@ -1,5 +1,5 @@
 package $package$.common.regex
 
 object Matchers {
-  val Email = """^[a-zA-Z0-9\.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"""
+  val Email = """^[a-zA-Z0-9\.!#\$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*\$"""
 }
