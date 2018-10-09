8aec6baa8c2c573131619fa8a0b6b6c2dc119e69
Robert D. Winter, 2nd <robert.d.winter.ctr@mail.mil>
2018-10-09 12:49:59 -0500

    feat(prototype): Giter8 template
    Template for creating new Lagom services.
    
    #NONE

---
+++ src/main/g8/$name__norm$-impl/src/main/scala/$package$/impl/domain/$name__Camel$Aggregate.scala
@@ -0,0 +1,73 @@
+package $package$.impl.domain
+
+import $package$.api.aggregate._
+import $package$.api.response._
+
+import akka.Done
+import com.lightbend.lagom.scaladsl.persistence.{AggregateEvent, AggregateEventTag, PersistentEntity}
+import com.lightbend.lagom.scaladsl.persistence.PersistentEntity.ReplyType
+import com.lightbend.lagom.scaladsl.playjson.{JsonSerializer, JsonSerializerRegistry}
+import java.time.LocalDateTime
+import play.api.libs.json.{Format, Json}
+import scala.collection.immutable.Seq
+
+
+object $name;format="Camel"$Status extends Enumeration {
+  val Created, Auction, Completed, Cancelled = Value
+  type Status = Value
+
+  implicit val format: Format[Status] = enumFormat($name;format="Camel"$Status)
+}
+
+case class $name;format="Camel"$(
+  id: UUID,
+  creator: UUID,
+  title: String,
+  description: String,
+  currencyId: String,
+  increment: Int,
+  reservePrice: Int,
+  price: Option[Int],
+  status: $name;format="Camel"$Status.Status,
+  auctionDuration: Duration,
+  auctionStart: Option[Instant],
+  auctionEnd: Option[Instant],
+  auctionWinner: Option[UUID]
+) {
+
+  def start(startTime: Instant) = {
+    assert(status == $name;format="Camel"$Status.Created)
+    copy(
+      status = $name;format="Camel"$Status.Auction,
+      auctionStart = Some(startTime),
+      auctionEnd = Some(startTime.plus(auctionDuration))
+    )
+  }
+
+  def end(winner: Option[UUID], price: Option[Int]) = {
+    assert(status == $name;format="Camel"$Status.Auction)
+    copy(
+      status = $name;format="Camel"$Status.Completed,
+      price = price,
+      auctionWinner = winner
+    )
+  }
+
+  def updatePrice(price: Int) = {
+    assert(status == $name;format="Camel"$Status.Auction)
+    copy(
+      price = Some(price)
+    )
+  }
+
+  def cancel = {
+    assert(status == $name;format="Camel"$Status.Auction || status == $name;format="Camel"$Status.Completed)
+    copy(
+      status = $name;format="Camel"$Status.Cancelled
+    )
+  }
+}
+
+object $name;format="Camel"$ {
+  implicit val format: Format[$name;format="Camel"$] = Json.format
+}
--- src/main/g8/$name__norm$-impl/src/main/scala/$package$/impl/domain/$name__Camel$Command.scala
+++ src/main/g8/$name__norm$-impl/src/main/scala/$package$/impl/domain/$name__Camel$Command.scala
@@ -14,11 +14,61 @@ import java.time.LocalDateTime
 import play.api.libs.json.{Format, Json}
 import scala.collection.immutable.Seq
 
+/**
+  * This is an event sourced entity. It has a state, [[$name;format="Camel"$State]], which
+  * stores what the greeting should be (eg, "Hello").
+  *
+  * Event sourced entities are interacted with by sending them commands. This
+  * entity supports two commands, a [[UseGreetingMessage]] command, which is
+  * used to change the greeting, and a [[Hello]] command, which is a read
+  * only command which returns a greeting to the name specified by the command.
+  *
+  * Commands get translated to events, and it's the events that get persisted by
+  * the entity. Each event will have an event handler registered for it, and an
+  * event handler simply applies an event to the current state. This will be done
+  * when the event is first created, and it will also be done when the entity is
+  * loaded from the database - each event will be replayed to recreate the state
+  * of the entity.
+  *
+  * This entity defines one event, the [[GreetingMessageChanged]] event,
+  * which is emitted when a [[UseGreetingMessage]] command is received.
+  */
 //You can interact with PersistentEntity by sending command messages to it. COMMANDS are instructions to do something.
 
 /**
   * This interface defines all the commands that the HelloWorld entity supports.
   */
+  
+sealed trait $name;format="Camel"$Command
+
+case object Get$name;format="Camel"$ extends $name;format="Camel"$Command with ReplyType[Option[$name;format="Camel"$]] {
+  implicit val format: Format[Get$name;format="Camel"$.type] = singletonFormat(Get$name;format="Camel"$)
+}
+
+case class Create$name;format="Camel"$($name;format="camel"$: $name;format="Camel"$) extends $name;format="Camel"$Command with ReplyType[Done]
+
+object Create$name;format="Camel"$ {
+  implicit val format: Format[Create$name;format="Camel"$] = Json.format
+}
+
+case class StartAuction(userId: UUID) extends $name;format="Camel"$Command with ReplyType[Done]
+
+object StartAuction {
+  implicit val format: Format[StartAuction] = Json.format
+}
+
+case class UpdatePrice(price: Int) extends $name;format="Camel"$Command with ReplyType[Done]
+
+object UpdatePrice {
+  implicit val format: Format[UpdatePrice] = Json.format
+}
+
+case class FinishAuction(winner: Option[UUID], price: Option[Int]) extends $name;format="Camel"$Command with ReplyType[Done]
+
+object FinishAuction {
+  implicit val format: Format[FinishAuction] = Json.format
+}
+
 sealed trait $name;format="Camel"$Command[R] extends ReplyType[R]
 
 /**
