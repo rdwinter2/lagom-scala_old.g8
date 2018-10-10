b1e8a76861b7a3b59efd05518e8485d71dfc741b
Robert D. Winter, 2nd <robert.d.winter.ctr@mail.mil>
2018-10-10 13:37:54 -0500

    feat(prototype): Giter8 template
    Template for creating new Lagom services.
    
    #NONE

--- src/main/g8/$name__norm$-impl/src/main/scala/$package$/impl/$name__Camel$ReadSideConnector.scala
+++ src/main/g8/$name__norm$-impl/src/main/scala/$package$/impl/$name__Camel$ReadSideConnector.scala
@@ -34,7 +34,7 @@ class $name;format="Camel"$ReadSideConnector(session: CassandraSession)(implicit
     session
       .selectAll(
           s"""
-            |SELECT $name;format="camel"$id FROM sessions WHERE access_token = '$access_token'
+            |SELECT $name;format="camel"$id FROM sessions WHERE access_token = '\$access_token'
           """.stripMargin)
       .map(_.headOption.toRight(ER.NotFound("Access token missing")).map(row => UUID.fromString(row.getString("$name;format="camel"$id"))))
   }
@@ -43,7 +43,7 @@ class $name;format="Camel"$ReadSideConnector(session: CassandraSession)(implicit
     session
       .selectAll(
         s"""
-           |SELECT $name;format="camel"$id FROM sessions WHERE refresh_token = '$refresh_token' ALLOW FILTERING
+           |SELECT $name;format="camel"$id FROM sessions WHERE refresh_token = '\$refresh_token' ALLOW FILTERING
           """.stripMargin)
       .map(_.headOption.toRight(ER.NotFound("Refresh token not found")).map(row => UUID.fromString(row.getString("$name;format="camel"$id"))))
   }
@@ -52,7 +52,7 @@ class $name;format="Camel"$ReadSideConnector(session: CassandraSession)(implicit
     session
       .selectAll(
         s"""
-           |SELECT id FROM $name;format="camel"$s WHERE $name;format="camel"$name = '$$name;format="camel"$name' ALLOW FILTERING
+           |SELECT id FROM $name;format="camel"$s WHERE $name;format="camel"$name = '\$$name;format="camel"$name' ALLOW FILTERING
           """.stripMargin)
       .map(_.headOption.toRight(ER.NotFound("$name;format="Camel"$name not found")).map(row => UUID.fromString(row.getString("id"))))
   }
@@ -61,7 +61,7 @@ class $name;format="Camel"$ReadSideConnector(session: CassandraSession)(implicit
     session
       .selectAll(
         s"""
-           |SELECT * FROM $name;format="camel"$s WHERE id = '${id.toString}'
+           |SELECT * FROM $name;format="camel"$s WHERE id = '\${id.toString}'
           """.stripMargin)
       .map(_.headOption
         .toRight(ER.NotFound("$name;format="Camel"$ not found with id"))
