68d2275e64ae84c89845c33128cfaa2a0f1fe774
Robert D. Winter, 2nd <robert.d.winter.ctr@mail.mil>
2018-10-31 12:24:14 -0500

    feat(prototype): Giter8 template
    Correct template for implementation test.
    
    #NONE

--- src/main/g8/$name__norm$-impl/src/test/scala/$package$/impl/$name__Camel$EntitySpec.scala
+++ src/main/g8/$name__norm$-impl/src/test/scala/$package$/impl/$name__Camel$EntitySpec.scala
@@ -4,9 +4,10 @@ import akka.actor.ActorSystem
 import akka.testkit.TestKit
 import com.lightbend.lagom.scaladsl.testkit.PersistentEntityTestDriver
 import com.lightbend.lagom.scaladsl.playjson.JsonSerializerRegistry
-import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpec}
+import java.util.UUID
+import org.scalatest.{BeforeAndAfterAll, Matchers, OptionValues, WordSpec}
 
-class $name;format="Camel"$EntitySpec extends WordSpec with Matchers with BeforeAndAfterAll {
+class $name;format="Camel"$EntitySpec extends WordSpec with Matchers with BeforeAndAfterAll with OptionValues {
 
   private val system = ActorSystem("$name;format="Camel"$EntitySpec",
     JsonSerializerRegistry.actorSystemSetupFor($name;format="Camel"$SerializerRegistry))
@@ -15,25 +16,32 @@ class $name;format="Camel"$EntitySpec extends WordSpec with Matchers with Before
     TestKit.shutdownActorSystem(system)
   }
 
-  private def withTestDriver(block: PersistentEntityTestDriver[$name;format="Camel"$Command[_], $name;format="Camel"$Event, $name;format="Camel"$State] => Unit): Unit = {
-    val driver = new PersistentEntityTestDriver(system, new $name;format="Camel"$Entity, "$name;format="norm"$-1")
-    block(driver)
-    driver.getAllIssues should have size 0
+  private val id = UUID.randomUUID
+  private val $name;format="camel"$ = $name;format="Camel"$(id, "name", "description")
+
+  private def withTestDriver[T](block: PersistentEntityTestDriver[$name;format="Camel"$Command, $name;format="Camel"$Event, $name;format="Camel"$State] => T): T = {
+    val driver = new PersistentEntityTestDriver(system, new $name;format="Camel"$Entity, id.toString)
+    try {
+      block(driver)
+    } finally {
+      driver.getAll$plural_name;format="Camel"$ shouldBe empty
+    }
   }
 
   "$name$ entity" should {
 
-    "say hello by default" in withTestDriver { driver =>
-      val outcome = driver.run(Hello("Alice"))
-      outcome.replies should contain only "Hello, Alice!"
+    "allow creating an $name$" in withTestDriver { driver =>
+      val outcome = driver.run(Create$name;format="Camel"$($name;format="camel"$))
+      outcome.events should contain only $name;format="Camel"$Created($name;format="camel"$)
+      outcome.state should ===(Some($name;format="camel"$))
     }
 
-    "allow updating the greeting message" in withTestDriver { driver =>
-      val outcome1 = driver.run(UseGreetingMessage("Hi"))
-      outcome1.events should contain only GreetingMessageChanged("Hi")
-      val outcome2 = driver.run(Hello("Alice"))
-      outcome2.replies should contain only "Hi, Alice!"
+    "allow looking up an $name$" in withTestDriver { driver =>
+      driver.run(Create$name;format="Camel"$($name;format="camel"$))
+      val outcome = driver.run(Get$name;format="Camel"$)
+      outcome.events shouldBe empty
+      outcome.replies should contain only Some($name;format="camel"$)
+      outcome.state should ===(Some($name;format="camel"$))
     }
-
   }
 }
--- src/main/g8/$name__norm$-impl/src/test/scala/$package$/impl/$name__Camel$ServiceSpec.scala
+++ src/main/g8/$name__norm$-impl/src/test/scala/$package$/impl/$name__Camel$ServiceSpec.scala
@@ -1,38 +1,125 @@
 package $package$.impl
 
-import com.lightbend.lagom.scaladsl.server.LocalServiceLocator
-import com.lightbend.lagom.scaladsl.testkit.ServiceTest
+import $package$.api
+import $package$.api.$name;format="Camel"$Service
+
+
+import akka.stream.scaladsl.Sink
+import akka.stream.testkit.scaladsl.TestSink
+import com.lightbend.lagom.scaladsl.server.{LagomApplication, LocalServiceLocator}
+import com.lightbend.lagom.scaladsl.testkit.{ServiceTest, TestTopicComponents}
 import org.scalatest.{AsyncWordSpec, BeforeAndAfterAll, Matchers}
-import $package$.api._
+import play.api.libs.ws.ahc.AhcWSComponents
+import scala.concurrent.duration._
+import scala.concurrent.{Future, Promise}
 
 class $name;format="Camel"$ServiceSpec extends AsyncWordSpec with Matchers with BeforeAndAfterAll {
 
   private val server = ServiceTest.startServer(
-    ServiceTest.defaultSetup
-      .withCassandra()
-  ) { ctx =>
-    new $name;format="Camel"$Application(ctx) with LocalServiceLocator
+    ServiceTest.defaultSetup.withCassandra(true)) { ctx =>
+    new LagomApplication(ctx) with $name;format="Camel"$Components with LocalServiceLocator with AhcWSComponents with TestTopicComponents
   }
 
-  val client = server.serviceClient.implement[$name;format="Camel"$Service]
+  val $name;format="camel"$Service: $name;format="Camel"$Service = server.serviceClient.implement[$name;format="Camel"$Service]
 
-  override protected def afterAll() = server.stop()
+  override protected def afterAll(): Unit = server.stop()
 
   "$name$ service" should {
-
-    "say hello" in {
-      client.hello("Alice").invoke().map { answer =>
-        answer should ===("Hello, Alice!")
-      }
-    }
-
-    "allow responding with a custom message" in {
+    "allow creating an $name$" in {
+      val input$name;format="Camel"$ = api.$name;format="Camel"$(None, "name", "description")
       for {
-        _ <- client.useGreeting("Bob").invoke(GreetingMessage("Hi"))
-        answer <- client.hello("Bob").invoke()
+        created$name;format="Camel"$ <- $name;format="camel"$Service.create$name;format="Camel"$.invoke(input$name;format="Camel"$)
       } yield {
-        answer should ===("Hi, Bob!")
+        created$name;format="Camel"$.safeId should not be null
+        created$name;format="Camel"$.name should be("name")
+        created$name;format="Camel"$.description should be("description")
       }
     }
+
+    "allow looking up a created $name$" in {
+      val input$name;format="Camel"$ = api.$name;format="Camel"$(None, "name", "description")
+      for {
+        created$name;format="Camel"$ <- $name;format="camel"$Service.create$name;format="Camel"$.invoke(input$name;format="Camel"$)
+        lookup$name;format="Camel"$ <- $name;format="camel"$Service.get$name;format="Camel"$(created$name;format="Camel"$.safeId).invoke
+      } yield {
+        created$name;format="Camel"$ should ===(lookup$name;format="Camel"$)
+      }
+    }
+
+    "allow looking up all created $name$" in {
+      val input$name;format="Camel"$1 = api.$name;format="Camel"$(None, "name1", "description1")
+      val input$name;format="Camel"$2 = api.$name;format="Camel"$(None, "name2", "description2")
+      val input$name;format="Camel"$3 = api.$name;format="Camel"$(None, "name3", "description3")
+      (for {
+        created$name;format="Camel"$1 <- $name;format="camel"$Service.create$name;format="Camel"$.invoke(input$name;format="Camel"$1)
+        created$name;format="Camel"$2 <- $name;format="camel"$Service.create$name;format="Camel"$.invoke(input$name;format="Camel"$2)
+        created$name;format="Camel"$3 <- $name;format="camel"$Service.create$name;format="Camel"$.invoke(input$name;format="Camel"$3)
+      } yield {
+        awaitSuccess() {
+          for {
+            lookup$name;format="Camel"$sResponse <- $name;format="camel"$Service.get$name;format="Camel"$s.invoke
+          } yield {
+            lookup$name;format="Camel"$sResponse.$name;format="camel"$s should contain allOf(created$name;format="Camel"$1, created$name;format="Camel"$2, created$name;format="Camel"$3)
+          }
+        }
+      }).flatMap(identity)
+    }
+
+    "publish $name;format="camel"$ events on the Kafka topic" in {
+      implicit val system = server.actorSystem
+      implicit val mat = server.materializer
+
+      for {
+        created$name;format="Camel"$ <- $name;format="camel"$Service.create$name;format="Camel"$.invoke(api.$name;format="Camel"$(None, "name", "description"))
+        events <- $name;format="camel"$Service.$name;format="camel"$Events.subscribe.atMostOnceSource
+          .filter(_.id == created$name;format="Camel"$.safeId)
+          .take(1)
+          .runWith(Sink.seq)
+      } yield {
+        events.size shouldBe 1
+        events.head shouldBe an[api.$name;format="Camel"$Created]
+        val event = events.head.asInstanceOf[api.$name;format="Camel"$Created]
+        event.name should be("name")
+        event.description should be("description")
+      }
+    }
+
+    "publish newly created $plural_name$ on the PubSub topic" in {
+      $name;format="camel"$Service.$name;format="camel"$Stream.invoke.map { source =>
+        implicit val system = server.actorSystem
+        implicit val mat = server.materializer
+
+        val $name;format="camel"$1 = api.$name;format="Camel"$(None, "name", "description")
+        val $name;format="camel"$2 = api.$name;format="Camel"$(None, "name2", "description2")
+        val $name;format="camel"$3 = api.$name;format="Camel"$(None, "name3", "description3")
+        val probe = source.runWith(TestSink.probe)
+        probe.request(3)
+
+        $name;format="camel"$Service.create$name;format="Camel"$.invoke($name;format="camel"$1)
+        $name;format="camel"$Service.create$name;format="Camel"$.invoke($name;format="camel"$2)
+        $name;format="camel"$Service.create$name;format="Camel"$.invoke($name;format="camel"$3)
+
+        probe.expectNextUnordered($name;format="camel"$1, $name;format="camel"$2, $name;format="camel"$3)
+        probe.cancel()
+        succeed
+      }
+    }
+  }
+
+  def awaitSuccess[T](maxDuration: FiniteDuration = 10.seconds, checkEvery: FiniteDuration = 100.milliseconds)(block: => Future[T]): Future[T] = {
+    val checkUntil = System.currentTimeMillis() + maxDuration.toMillis
+
+    def doCheck(): Future[T] = {
+      block.recoverWith {
+        case recheck if checkUntil > System.currentTimeMillis() =>
+          val timeout = Promise[T]()
+          server.application.actorSystem.scheduler.scheduleOnce(checkEvery) {
+            timeout.completeWith(doCheck())
+          }(server.executionContext)
+          timeout.future
+      }
+    }
+
+    doCheck()
   }
 }
