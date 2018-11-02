f787727d538c129218f09833223ac6354ef05647
Robert D. Winter, 2nd <robert.d.winter.ctr@mail.mil>
2018-11-02 14:14:11 -0500

    feat(prototype): perl program to generate code for schema definitions
    perl program
    
    #AMDAP-9

---
+++ src/main/g8/schemagen.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/env perl
+
+# Read schema definition file
+
+sub kabab {$s = join('-', map {lc $_} split(/(?=[A-Z])/, $_[0]));}
+sub snake {$s = join('_', map {lc $_} split(/(?=[A-Z])/, $_[0]));}
+sub word {$s = join(' ', map {ucfirst $_} split(/(?=[A-Z])/, $_[0]));}
+sub SNAKE {uc(snake($_[0]));}
+sub camel {lcfirst $_[0];}
+sub Camel {ucfirst $_[0];}
+
+$a1 = 'MyCamelCase';
+
+my $j = kabab($a1);
+my $k = snake($a1);
+my $i = camel($a1);
+my $m = Camel($a1);
+my $n = SNAKE($a1);
+my $w = word($a1);
+
+print "$a1 $j $k $i $m $n $w\n";
