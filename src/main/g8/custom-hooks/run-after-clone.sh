7e4c68ce6d8aaa138a770552aa91e11e532621ec
Robert D. Winter, 2nd <robert.d.winter.ctr@mail.mil>
2018-12-06 06:55:20 -0600

    feat(template): add authz
    Removed streaming for now.
    
    #AMDAP-10

--- src/main/g8/custom-hooks/run-after-clone.sh
+++ src/main/g8/custom-hooks/run-after-clone.sh
@@ -5,4 +5,4 @@ toplevel=`git rev-parse --show-toplevel`
 #present_working_directory=`pwd`
 
 # create a link from custom-hooks to .git/hooks
-ln -s ${toplevel}/custom-hooks/pre-commit ${toplevel}/.git/hooks/pre-commit
+ln -s \${toplevel}/custom-hooks/pre-commit \${toplevel}/.git/hooks/pre-commit
