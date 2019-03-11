1f32679414077a01cd1bc8e1216878302829336e
Robert D. Winter, 2nd <rdwinter2@gmail.com>
2019-03-11 15:43:05 -0500

    feat(template): add authz
    Removed streaming for now.
    
    #AMDAP-10

--- src/main/g8/common/src/main/scala/$organization__packaged$/common/authentication/TokenContent.scala
+++ src/main/g8/common/src/main/scala/$organization__packaged$/common/authentication/TokenContent.scala
@@ -4,6 +4,8 @@ import java.util.UUID
 
 import play.api.libs.json.{Format, Json}
 
+// [JWT(https://tools.ietf.org/html/rfc7519)
+
 case class TokenContent(clientId: UUID, userId: UUID, username: String, isRefreshToken: Boolean = false)
 object TokenContent {
   implicit val format: Format[TokenContent] = Json.format
